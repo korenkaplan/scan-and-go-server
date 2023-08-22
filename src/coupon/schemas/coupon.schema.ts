559ae90eabba8be879663e67bff5c13aef70e348
koren_kaplan <korenkaplan96@gmail.com>
2023-08-22 12:10:54 +0300

    Created schemas coupon and inactive user

---
+++ src/coupon/schemas/coupon.schema.ts
@@ -0,0 +1,49 @@
+import { Schema,SchemaFactory, Prop } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+
+@Schema({
+    timestamps:{createdAt:true, updatedAt:false},
+    collection:'coupons',
+})
+export class Coupon extends Document {
+    @Prop()
+    code: string; // The coupon code 
+  
+    @Prop()
+    discountPercentage: number; // The discount percentage
+  
+    @Prop()
+    validFrom: Date; // The start date of coupon validity
+  
+    @Prop()
+    validUntil: Date; // The end date of coupon validity
+  
+    @Prop()
+    maxUsageCount: number; // Maximum number of times the coupon can be used
+  
+    @Prop()
+    currentUsageCount: number; // Current usage count
+  
+    @Prop()
+    isActive: boolean; // Whether the coupon is active
+  
+    @Prop()
+    createdAt: Date; // Timestamp of when the coupon was created
+
+    @Prop()
+    schemaVersion: number; // Version of the schema
+
+}
+export interface ICoupon {
+    _id?: mongoose.Types.ObjectId;
+    code: string;
+    discountPercentage: number;
+    validFrom: Date;
+    validUntil: Date;
+    maxUsageCount: number;
+    currentUsageCount: number;
+    isActive: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+  export const CouponSchema = SchemaFactory.createForClass(Coupon);
---
+++ src/user/schemas/inactive-user.schema.ts
@@ -0,0 +1,101 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { Document } from "mongoose";
+import { Role } from "utils/enums/roles.enum";
+import { CreditCard } from "./credit-card.schema";
+import { ItemInCart } from "./item-in-cart.schema";
+import { Gender } from "utils/enums/gender.enum";
+import { RecentItem } from "./recent-items.schema";
+import { RecentTransactions } from "./recent-transactions.schema";
+
+@Schema({
+    timestamps:{createdAt:true, updatedAt:false},
+    collection:'inactiveUsers',
+})
+
+export class InactiveUser extends Document {
+    @Prop()
+    fullName:string;
+
+    @Prop()
+    roles: Role[]
+
+    @Prop({unique:true})
+    email: string;
+
+    @Prop()
+    creditCards: CreditCard[];
+
+    @Prop()
+    password: string;
+
+    @Prop()
+    cart: ItemInCart[]
+
+    @Prop()
+    gender:Gender
+
+    @Prop()
+    birthDate: Date
+
+    @Prop()
+    isActive: boolean
+
+    @Prop()
+    schemaVersion: number
+
+    @Prop()
+    deviceToken: string
+    
+    @Prop()
+    createdAt: Date
+
+    @Prop()
+    transactionsAmount: number
+
+    @Prop({index:1}) // Create an index on lastActivity field, sorted in ascending order (oldest to newest)
+    lastActivity: Date
+
+    @Prop()
+    recentItems: RecentItem[]
+
+    @Prop()
+    recentTransactions: RecentTransactions[]
+}   
+export interface IinactiveUser  {
+
+    _id?:mongoose.Types.ObjectId;
+
+    fullName:string;
+
+    roles: Role[]
+
+    email: string;
+
+    creditCards: CreditCard[];
+
+    password: string;
+
+    cart: ItemInCart[]
+
+    gender:Gender
+
+    birthDate: Date
+
+    isActive: boolean
+
+    schemaVersion: number
+
+    deviceToken: string
+    
+    createdAt: Date
+
+    transactionsAmount: number
+
+    lastActivity: Date
+
+    recentItems: RecentItem[]
+
+    recentTransactions: RecentTransactions[]
+}   
+
+export const InactiveUserSchema = SchemaFactory.createForClass(InactiveUser)
