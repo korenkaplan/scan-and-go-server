8bee32928fc4af44a38ccc4f433f82e8350325db
koren_kaplan <korenkaplan96@gmail.com>
2023-08-23 14:45:56 +0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,10 +18,10 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   envFilePath: '.env',
-    //   isGlobal: true
-    // }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
     // MongooseModule.forRoot(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}${process.env.MONGO_PATH}`,{dbName: process.env.DB_NAME}),
     // UserModule,
     // AuthModule,
