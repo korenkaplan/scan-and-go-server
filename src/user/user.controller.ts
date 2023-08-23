a1fb2960f202d1d292bce795ae5532b488d50620
koren_kaplan <korenkaplan96@gmail.com>
2023-08-23 19:28:25 +0300

    sadasds

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,16 +9,29 @@ import { UpdateQueryDto } from 'src/user/global-dto/update-query.dto';
 export class UserController {
     constructor(private userService: UserService) { }
     @Get('/getAll')
-    async getUsers(@Body() dto: GetQueryDto<User>): Promise<User[]> {
+    async getUsers(@Body() dto): Promise<User[]> {
         return await this.userService.getMany(dto);
     }
     @Delete('/deleteAll')
-    async deleteUsers(@Body() query: FilterQuery<User>): Promise<number> {
+    async deleteUsers(@Body() query): Promise<number> {
         return await this.userService.deleteMany(query);
     }
 
     @Put('/updateAll')
-    async updateUsers(@Body() dto: UpdateQueryDto<User>): Promise<number> {
+    async updateUsers(@Body() dto): Promise<number> {
         return await this.userService.updateMany(dto);
     }
+    // @Get('/getAll')
+    // async getUsers(@Body() dto: GetQueryDto<User>): Promise<User[]> {
+    //     return await this.userService.getMany(dto);
+    // }
+    // @Delete('/deleteAll')
+    // async deleteUsers(@Body() query: FilterQuery<User>): Promise<number> {
+    //     return await this.userService.deleteMany(query);
+    // }
+
+    // @Put('/updateAll')
+    // async updateUsers(@Body() dto: UpdateQueryDto<User>): Promise<number> {
+    //     return await this.userService.updateMany(dto);
+    // }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -13,7 +13,6 @@ export class UserService {
         @InjectModel(User.name)
         private userModel: mongoose.Model<User>
     ) { }
-
     async updatePassword(dto: UpdatePasswordDto): Promise<void> {
         const { oldPassword, newPassword, userId } = dto;
 
@@ -38,22 +37,22 @@ export class UserService {
         return hashedPassword
     }
     //#region CRUD OPERATIONS
-    async getMany(dto: GetQueryDto<User>): Promise<User[]> {
+    async getMany(dto): Promise<User[]> {
         const { query, projection } = dto
 
         const users = await this.userModel.find(query, projection);
         return users
     }
-    async getOne(dto: GetQueryDto<User>): Promise<User> {
+    async getOne(dto): Promise<User> {
         const { query, projection } = dto
 
         return await this.userModel.findOne(query, projection);
     }
-    async updateOne(dto: UpdateQueryDto<User>): Promise<User> {
+    async updateOne(dto): Promise<User> {
         const { query, updateQuery } = dto
         return await this.userModel.findOneAndUpdate(query, updateQuery, { new: true });
     }
-    async updateMany(dto: UpdateQueryDto<User>): Promise<number> {
+    async updateMany(dto): Promise<number> {
         const { query, updateQuery } = dto
         const result: UpdateWriteOpResult = await this.userModel.updateMany(query, updateQuery, { new: true });
         return result.modifiedCount
@@ -66,4 +65,56 @@ export class UserService {
 
     }
     //#endregion
+    // async updatePassword(dto: UpdatePasswordDto): Promise<void> {
+    //     const { oldPassword, newPassword, userId } = dto;
+
+    //     //find the user with the id
+    //     const user = await this.userModel.findById(userId);
+
+    //     //if not found throw error
+    //     if (!user) throw new NotFoundException('No user with id ' + userId + ' found in database');
+
+    //     //hash oldPassword and compare to user.password
+    //     const isPasswordMatch = await bcrypt.compare(oldPassword, user.password);
+
+    //     //if not match return return old password incorrect
+    //     if (!isPasswordMatch) throw new BadRequestException('Old password does not match')
+
+    //     //hash new password and update user password
+    //     user.password = await this.hashPassword(newPassword)
+    //     await user.save()
+    // }
+    // async hashPassword(password: string): Promise<string> {
+    //     const hashedPassword = await bcrypt.hash(password, 10);
+    //     return hashedPassword
+    // }
+    // //#region CRUD OPERATIONS
+    // async getMany(dto: GetQueryDto<User>): Promise<User[]> {
+    //     const { query, projection } = dto
+
+    //     const users = await this.userModel.find(query, projection);
+    //     return users
+    // }
+    // async getOne(dto: GetQueryDto<User>): Promise<User> {
+    //     const { query, projection } = dto
+
+    //     return await this.userModel.findOne(query, projection);
+    // }
+    // async updateOne(dto: UpdateQueryDto<User>): Promise<User> {
+    //     const { query, updateQuery } = dto
+    //     return await this.userModel.findOneAndUpdate(query, updateQuery, { new: true });
+    // }
+    // async updateMany(dto: UpdateQueryDto<User>): Promise<number> {
+    //     const { query, updateQuery } = dto
+    //     const result: UpdateWriteOpResult = await this.userModel.updateMany(query, updateQuery, { new: true });
+    //     return result.modifiedCount
+    // }
+    // async deleteOne(query: FilterQuery<User>): Promise<User> {
+    //     return await this.userModel.findOneAndDelete(query);
+    // }
+    // async deleteMany(query: FilterQuery<User>): Promise<number> {
+    //     return (await this.userModel.deleteMany(query)).deletedCount;
+
+    // }
+    // //#endregion
 }
