0cf39350dc8acfd9af1cd191b45a17b86336dd62
koren_kaplan <korenkaplan96@gmail.com>
2023-08-23 21:48:12 +0300

    test

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Transaction } from './schemas/transaction.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { GetQueryDto } from './transaction.dto';
+import { GetQueryDto } from 'src/global/global.dto';
 
 @Injectable()
 export class TransactionsService {
