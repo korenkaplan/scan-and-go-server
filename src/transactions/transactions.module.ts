c030916cc1daf77b0e50b0be6a312115222f6e5f
koren_kaplan <korenkaplan96@gmail.com>
2023-08-23 16:08:51 +0300

    aaaaa

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -6,11 +6,11 @@ import { Transaction, TransactionSchema } from './schemas/transaction.schema';
 
 @Module({
   //imports:[MongooseModule.forFeature([{name:Transaction.name,schema:TransactionSchema}])],
-  imports:[MongooseModule.forFeatureAsync([{name:Transaction.name,useFactory: () => {
-  const schema = TransactionSchema;
-  schema.loadClass(Transaction);
-  return schema
-  }}])],
+  // imports:[MongooseModule.forFeatureAsync([{name:Transaction.name,useFactory: () => {
+  // const schema = TransactionSchema;
+  // schema.loadClass(Transaction);
+  // return schema
+  // }}])],
   providers: [TransactionsService],
   controllers: [TransactionsController],
   exports:[TransactionsService]
