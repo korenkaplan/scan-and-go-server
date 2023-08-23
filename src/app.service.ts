c2a9d8f148fcadd704c07b8a9c8f3f42f8888f2b
koren_kaplan <korenkaplan96@gmail.com>
2023-08-23 18:12:52 +0300

    ss

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `mongodb+srv:// + ${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}${process.env.MONGO_PATH}`;
+    return `Hello World`;
   }
 }
