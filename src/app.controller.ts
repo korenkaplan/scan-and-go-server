fb06e04e9ec7d4bac94c896c63dec527d5c10670
koren_kaplan <korenkaplan96@gmail.com>
2023-08-23 13:23:34 +0300

    public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Public } from './auth/decorators/public-guard.decorator';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello(); //addd
